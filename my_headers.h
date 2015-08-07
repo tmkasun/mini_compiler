8e6282de436db72de97925ca2678450b36874b13
tmkasun <tmkasun@gmail.com>
2015-08-07 15:54:38 +0530

    Define used tokens

---
+++ my_headers.h
@@ -0,0 +1,14 @@
+// These are the different type of tokens that scanner has to recognize
+
+// Following are the keywords in the language (sample_db_config file)
+#define NAME 1
+#define TYPE 2
+#define PORT 3
+#define COLLECTION 4
+#define USER 5
+
+#define COLON 6
+#define IDENTIFIER 7
+#define INTEGER 8
+#define NUMBER 9
+
